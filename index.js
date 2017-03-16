872bdf4bfea070df345106657f82a77773bd1f74
Denis Sokolov <dnssokolov@gmail.com>
2017-03-16 23:35:09 +0300

    disable import/no-extraneous-dependencies eslint rule

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-/* eslint import/no-extraneous-dependencies:0 */
 // use babel-register only for local environment, build scripts for production
-
 require('babel-register');
+
 require('./server/server');
